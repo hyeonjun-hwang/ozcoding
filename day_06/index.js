62760b24eb525a5ba80495207a1b15a6ac2fb9a5
hwanga <hwanga555@gmail.com>
2025-09-30 16:32:30 +0900

    6th day

--- day_06/index.js
+++ day_06/index.js
@@ -4,3 +4,18 @@ console.log(score);
 // console.log(aaa);
 let aaa;
 console.log(aaa);
+
+let num1 = 123;
+let num2;
+
+console.log(`num1은 ${num1} 이다`);
+
+console.log(`0 / 0은 ${0 / 0}`);
+
+console.log(typeof num1);
+
+console.log(`num1 + undefined is ${num1 + undefined}`);
+
+console.log(typeof (num1 + undefined));
+
+console.log(typeof num2);
