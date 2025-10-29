e68484fc4b248b456d70ff55bcf60332004029a9
hwanga <hwanga555@gmail.com>
2025-10-29 15:29:43 +0900

    [feat] todos sorting

--- study_js/day_18/day_18.js
+++ study_js/day_18/day_18.js
@@ -9,7 +9,7 @@
 // => "조회 가능한 할 일이 없습니다.", "할 일을 추가하세요!" 등 => 최소 높이를 정해주면 좋을 것 같다?
 // ✅ 8. 스크롤 처리
 // ✅ 9. 공백으로 작성 후 할 일 추가할 시, 경고 문구 띄우기
-// 10. #뒤에 있는 숫자가 우선순위를 의미한다라고 가정하고, 순차적으로 보여준다.
+// ✅ 10. #뒤에 있는 숫자가 우선순위를 의미한다라고 가정하고, 순차적으로 보여준다.
 
 // 요소 추출
 const inputField = document.querySelector(".input-group input");
@@ -63,6 +63,8 @@ function renderTodos() {
   // 3. 추출한 그 숫자를 가지고 배열을 sort 한다
   // console.log("todos :", todos);
 
+  console.log("before :", todos);
+
   // inputValue #n 추출용 함수
   function getSortNum(str) {
     const match = str.match(/#(\d+)/);
@@ -72,13 +74,18 @@ function renderTodos() {
       return parseInt(match[1], 10);
     }
   }
-  // 작업중 ...
-  //   todos.sort((a,b) => {
-  //     const numA = getSortNum(a.b)
-  //   })
-  //   console.log(getSortNum("asd #2"));
-  //
+  console.log(getSortNum("asdsad"));
 
+  // 배열을 #n 순서에 맞게 재배열 해서 반환 (내림차순)
+  todos.sort((a, b) => {
+    const numA = getSortNum(a.inputValue);
+    const numB = getSortNum(b.inputValue);
+    return numB - numA;
+  });
+
+  console.log("after: ", todos);
+
+  // todos 순회하면서 화면 그리기
   todos.forEach((todo, index) => {
     // todo list 요소 생성 및 클래스 할당
     const todoItem = document.createElement("div");
